4540b34c3fa2e5ef9db2d8c93a342137232f7d33
denis256 <denis.o@linux.com>
2023-12-31 23:57:42 +0200

    Metrics colleciton simplification

--- unidev-prometheus/src/main/java/com/unidev/platform/prometheus/MetricService.java
+++ unidev-prometheus/src/main/java/com/unidev/platform/prometheus/MetricService.java
@@ -62,20 +62,22 @@ public class MetricService {
     public Counter.Child counter(String name, Map<String, String> labels) {
         try {
             TreeMap<String, String> labelMap = new TreeMap<>(labels);
+            labelMap.put("labelName", name);
             labelMap.putAll(defaultLabels);
             String prometheusName = buildName(name);
-            counters.computeIfAbsent(prometheusName, s -> {
-                Counter value = Counter.build()
-                        .name(prometheusName)
-                        .labelNames(mapKeys(labelMap))
-                        .help("Counter " + name).register();
-                return value;
-            });
+            counters.computeIfAbsent(prometheusName, s -> Counter.build()
+                    .name(prometheusName)
+                    .labelNames(mapKeys(labelMap))
+                    .help("Counter " + name).register());
             return counters.get(prometheusName).labels(mapValues(labelMap));
         } catch (Throwable t) {
             t.printStackTrace();
         }
         return new Counter.Child();
+    }
+
+    public Counter.Child counter(String name) {
+        return counter(name, Map.of());
     }
 
     /**
@@ -85,22 +87,24 @@ public class MetricService {
         counter(name, labels).inc();
     }
 
+    public void inc(String name) {
+        counter(name, Map.of()).inc();
+    }
+
     /**
      * Register Gauge
      */
     public Gauge.Child gauge(String name, Map<String, String> labels) {
         TreeMap<String, String> labelMap = new TreeMap<>(labels);
+        labelMap.put("labelName", name);
         labelMap.putAll(defaultLabels);
         try {
             String prometheusName = buildName(name);
-            gauges.computeIfAbsent(prometheusName, s -> {
-                Gauge value = Gauge.build()
-                        .name(prometheusName)
-                        .help("Gauge " + prometheusName)
-                        .labelNames(mapKeys(labelMap))
-                        .register();
-                return value;
-            });
+            gauges.computeIfAbsent(prometheusName, s -> Gauge.build()
+                    .name(prometheusName)
+                    .help("Gauge " + prometheusName)
+                    .labelNames(mapKeys(labelMap))
+                    .register());
             return gauges.get(prometheusName).labels(mapValues(labelMap));
         }catch (Throwable t) {
             t.printStackTrace();
@@ -108,22 +112,24 @@ public class MetricService {
         return Gauge.build().create().labels();
     }
 
+    public Gauge.Child gauge(String name) {
+        return gauge(name, Map.of());
+    }
+
     /**
      * Build summary
      */
     public Summary.Child summary(String name, Map<String, String> labels) {
         TreeMap<String, String> labelMap = new TreeMap<>(labels);
+        labelMap.put("labelName", name);
         labelMap.putAll(defaultLabels);
         try {
             String prometheusName = buildName(name);
-            summaries.computeIfAbsent(prometheusName, s -> {
-                Summary value = Summary.build()
-                        .name(prometheusName)
-                        .help("Summary " + prometheusName)
-                        .labelNames(mapKeys(labelMap))
-                        .register();
-                return value;
-            });
+            summaries.computeIfAbsent(prometheusName, s -> Summary.build()
+                    .name(prometheusName)
+                    .help("Summary " + prometheusName)
+                    .labelNames(mapKeys(labelMap))
+                    .register());
             return summaries.get(prometheusName).labels(mapValues(labelMap));
         }catch (Throwable t) {
             t.printStackTrace();
@@ -131,22 +137,24 @@ public class MetricService {
         return Summary.build().create().labels(mapValues(labelMap));
     }
 
+    public Summary.Child summary(String name) {
+        return summary(name, Map.of());
+    }
+
     /**
      * Register histogram
      */
     public Histogram.Child histogram(String name, Map<String, String> labels) {
         TreeMap<String, String> labelMap = new TreeMap<>(labels);
+        labelMap.put("labelName", name);
         labelMap.putAll(defaultLabels);
         try {
             String prometheusName = buildName(name);
-            histograms.computeIfAbsent(prometheusName, s -> {
-                Histogram value = Histogram.build()
-                        .name(prometheusName)
-                        .help("Histogram " + prometheusName)
-                        .labelNames(mapKeys(labelMap))
-                        .register();
-                return value;
-            });
+            histograms.computeIfAbsent(prometheusName, s -> Histogram.build()
+                    .name(prometheusName)
+                    .help("Histogram " + prometheusName)
+                    .labelNames(mapKeys(labelMap))
+                    .register());
             return histograms.get(prometheusName).labels(mapValues(labelMap));
         }catch (Throwable t) {
             t.printStackTrace();
@@ -154,15 +162,24 @@ public class MetricService {
         return Histogram.build().create().labels(mapValues(labelMap));
     }
 
+    public Histogram.Child histogram(String name) {
+        return histogram(name, Map.of());
+    }
+
     /**
      * Count exceptions.
      */
     public void countException(Throwable t, Map<String, String> labels) {
-        TreeMap<String, String> labelMap = new TreeMap<>(labels);
-        labelMap.putAll(defaultLabels);
         String name = t.getClass().getCanonicalName();
+        TreeMap<String, String> labelMap = new TreeMap<>(labels);
+        labelMap.put("labelName", name);
+        labelMap.putAll(defaultLabels);
         labelMap.put("exception", name);
         counter("exception", labelMap).inc();
+    }
+
+    public void countException(Throwable t) {
+        countException(t, Map.of());
     }
 
     String[] mapKeys(Map<String, String> map) {
